2ef26e3c864a7bee54f5fafeee5502f3738047b9
likinio365 <140794807+likinio365@users.noreply.github.com>
2026-01-29 12:13:05 +0200

    Update restore.sh

--- Mongo/restore.sh
+++ Mongo/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Ξεκινάει η αυτόματη επαναφορά της βάσης..."
 
-# Χρησιμοποιούμε τις μεταβλητές περιβάλλοντος που έχει ήδη το container
+
 mongorestore \
   --username "$MONGO_INITDB_ROOT_USERNAME" \
   --password "$MONGO_INITDB_ROOT_PASSWORD" \
